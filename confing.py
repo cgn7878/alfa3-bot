60a4620f1cb145293baf776559da9d5216c85365
cgn7878 <cagancifci@yandex.com>
2025-07-19 16:25:28 +0300

    Create confing.py

---
+++ confing.py
@@ -0,0 +1,22 @@
+# --- TELEGRAM BOT AYARLARI ---
+BOT_TOKEN = "7981029346:AAHzDEJtHmZt8V0ZEt7JoT59stlKn1_4DKk"
+CHAT_ID = "7831759991"
+
+# --- ZAMANLAYICI AYARI ---
+DONGU_SANIYE = 60  # ⏱️ Alfa 3'te anlık (1 saniye) yerine 60 saniyede bir çalışır. Bu değeri gerekirse 30 yapabilirsin.
+
+# --- ANALİZ AYARLARI ---
+DEFAULT_COIN = "bitcoin"
+DEFAULT_SYMBOL = "btc"
+VS_CURRENCY = "usd"
+COIN_LIMIT = 40  # CoinGecko sıralamasına göre ilk 40 coin analiz edilir.
+
+# --- GÖSTERGE EŞİKLERİ ---
+RSI_AL = 30
+RSI_SAT = 70
+
+# --- TELEGRAM BAŞLIK KODLARI ---
+BASLIK_AL = "📈 <b>Acil Al❗️</b>"
+BASLIK_SAT = "📉 <b>Acil Sat❗️</b>"
+BASLIK_HABER = "🗞️ <b>Kripto Haber</b>"
+BASLIK_RAPOR = "🕒 <b>30 Dakikalık Rapor</b>"
